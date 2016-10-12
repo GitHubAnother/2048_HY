70c3a7ee7eec060899c8460a6af330f8fcf8dde8
GitHubAnother <1041820154@qq.com>
2016-10-12 16:48:37 +0800

    1

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,361 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+    #region +
+    public GameObject numberPrefab;//数字的预设
+    public GameObject welcomePage;//欢迎界面
+    public GameObject gameWinPage;//游戏胜利界面
+    public GameObject gameLosePage;//游戏失败界面
+    public GameObject exitMessagePrefab;//退出游戏的提示
+    public Texture musicOn;//播放音乐时显示的图片
+    public Texture musicOff;//不播放音乐时显示的图片
+    public UITexture music;//显示播放音乐的UITexture
+
+    public Number[,] Numbers = new Number[4, 4];//保存方格中数字的数组
+    [HideInInspector]
+    public List<Number> IsMovingNumbers = new List<Number>();//正在移动中的数字集合
+    [HideInInspector]
+    public bool hasMove = false;//判断时候有数字发生了移动
+    [HideInInspector]
+    public bool canControl = false;//游戏是否可以控制
+    #endregion
+
+    #region -
+    private AudioSource audioSource;
+    private bool playerMusic;//是否播放背景音乐
+    private GameObject exitMessage;//退出游戏的实例化对象
+    #endregion
+
+    #region 单例
+    public static Manager _i;
+
+    void Awake()
+    {
+        _i = this;
+    }
+    #endregion
+
+    #region Unity内置函数
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+
+        Instantiate(numberPrefab);
+        Instantiate(numberPrefab);
+
+        playerMusic = PlayerPrefs.GetInt("playMusic", 1) == 1 ? true : false;
+
+        if (playerMusic)
+        {
+            audioSource.Play();
+            music.mainTexture = musicOn;
+        }
+        else
+        {
+            audioSource.Stop();
+            music.mainTexture = musicOff;
+        }
+    }
+    void Update()
+    {
+        //手机上的返回键对应的是PC上的ESC键
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (exitMessage == null)
+            {
+                exitMessage = Instantiate(exitMessagePrefab) as GameObject;
+                StartCoroutine("reaetQuitMessage");
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+        if (canControl && IsMovingNumbers.Count == 0)//没有数字正在移动的时候才能执行这里的操作
+        {
+            int dirX = 0;
+            int dirY = 0;
+
+            if (Input.GetKeyDown("up"))
+            {
+                dirY = 1;
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+                dirY = -1;
+            }
+            else if (Input.GetKeyDown("left"))
+            {
+                dirX = -1;
+            }
+            else if (Input.GetKeyDown("right"))
+            {
+                dirX = 1;
+            }
+
+            //移动端的触屏控制游戏
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+                if (Mathf.Abs(touchDeltaPosition.x) > Mathf.Abs(touchDeltaPosition.y))
+                {
+                    if (touchDeltaPosition.x > 10)
+                    {
+                        dirX = 1;
+                    }
+                    else if (touchDeltaPosition.x < -10)
+                    {
+                        dirX = -1;
+                    }
+                }
+                else
+                {
+                    if (touchDeltaPosition.y > 10)
+                    {
+                        dirY = 1;
+                    }
+                    else if (touchDeltaPosition.y < -10)
+                    {
+                        dirY = -1;
+                    }
+                }
+            }
+
+            MoveNumbers(dirX, dirY);
+        }
+
+        //每次移动完后实例化一个新的数字
+        if (hasMove && IsMovingNumbers.Count == 0)
+        {
+            Instantiate(numberPrefab);
+            hasMove = false;
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    if (Numbers[x, y] != null)
+                    {
+                        Numbers[x, y].hasMixed = false;
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region +CloseWelcomeToPlayGameScene关闭欢迎界面跳转到游戏场景
+    public void CloseWelcomeToPlayGameScene()
+    {
+        canControl = true;
+        welcomePage.SetActive(false);
+    }
+    #endregion
+
+    #region +MoveNumbers控制数字的移动,不实现数字的合并
+    public void MoveNumbers(int dirX, int dirY)
+    {
+        if (dirX == 1)//右
+        {
+            for (int y = 0; y < 4; y++)//到4表示4行都能向右移动
+            {
+                for (int x = 2; x >= 0; x--)//从2开始，表示除了最右边的一列其余3列都能向右移动
+                {
+                    if (Numbers[x, y] != null)
+                    {
+                        Numbers[x, y].Move(dirX, dirY);
+                    }
+                }
+            }
+        }
+        else if (dirX == -1)//左
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 1; x < 4; x++)
+                {
+                    if (Numbers[x, y] != null)
+                    {
+                        Numbers[x, y].Move(dirX, dirY);
+                    }
+                }
+            }
+        }
+        else if (dirY == 1)//上
+        {
+            for (int x = 0; x < 4; x++)//向上有4列都可以移动
+            {
+                for (int y = 2; y >= 0; y--)//但只有除最上面的那一行的其余3行才能移动
+                {
+                    if (Numbers[x, y] != null)
+                    {
+                        Numbers[x, y].Move(dirX, dirY);
+                    }
+                }
+            }
+        }
+        else if (dirY == -1)//下
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 1; y < 4; y++)//除了最下面一行其余3行都能移动
+                {
+                    if (Numbers[x, y] != null)
+                    {
+                        Numbers[x, y].Move(dirX, dirY);
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region +isEmpty用来判断当前传进来的这个位置上有没有数字
+    public bool isEmpty(int x, int y)
+    {
+        if (x < 0 || x >= 4 || y < 0 || y >= 4)//超出了4X4格子边界
+        {
+            return false;//返回false表示不为空
+        }
+        else if (Numbers[x, y] != null)//当前这个位置上有数字
+        {
+            return false;//也是返回不为空
+        }
+        else//除此之外全部都是空的
+        {
+            return true;//表示当前传进来的这个位置是可以用的
+        }
+    }
+    #endregion
+
+    #region +ToggleMusc用来控制音乐的播放
+    public void ToggleMusc()
+    {
+        if (playerMusic)
+        {
+            playerMusic = false;
+            audioSource.Stop();
+            music.mainTexture = musicOff;
+            PlayerPrefs.SetInt("playMusic", 0);
+        }
+        else
+        {
+            playerMusic = true;
+            audioSource.Play();
+            music.mainTexture = musicOn;
+            PlayerPrefs.SetInt("playMusic", 1);
+        }
+    }
+    #endregion
+
+    #region +Restart重新开始游戏
+    public void Restart()
+    {
+        canControl = true;
+
+        gameLosePage.SetActive(false);
+        gameWinPage.SetActive(false);
+
+        //清空界面上的所有数字
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                if (Numbers[x, y] != null)
+                {
+                    Destroy(Numbers[x, y].gameObject);
+                    Numbers[x, y] = null;
+                }
+            }
+        }
+
+        Instantiate(numberPrefab);
+        Instantiate(numberPrefab);
+    }
+    #endregion
+
+    #region +GameWin游戏胜利
+    public void GameWin()
+    {
+        canControl = false;
+        gameWinPage.SetActive(true);
+    }
+    #endregion
+
+    #region +ContinueGame继续游戏
+    public void ContinueGame()
+    {
+        canControl = true;
+        gameWinPage.SetActive(false);
+    }
+    #endregion
+
+    #region +GameLose游戏失败
+    public void GameLose()
+    {
+        canControl = false;
+        gameLosePage.SetActive(true);
+    }
+    #endregion
+
+    #region +isDead判断游戏失败
+    public bool isDead()
+    {
+        //先判断还有没有空的格子
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                if (Numbers[x, y] == null)
+                {
+                    return false;
+                }
+            }
+        }
+
+        //判断时候还有可以合并的数字---判断行
+        for (int y = 0; y < 4; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                if (Numbers[x, y].numberValue == Numbers[x + 1, y].numberValue)
+                {
+                    return false;
+                }
+            }
+        }
+
+        //判断列上还有没可以合并的数字
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                if (Numbers[x, y].numberValue == Numbers[x, y + 1].numberValue)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;//游戏失败
+    }
+    #endregion
+
+    #region reaetQuitMessage协程---手机上单击一次返回键会提示一下，连续单击2次才会退出游戏
+    IEnumerator reaetQuitMessage()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        if (exitMessage != null)
+        {
+            Destroy(exitMessage);
+        }
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/Number.cs
@@ -0,0 +1,261 @@
+﻿using UnityEngine;
+
+public class Number : MonoBehaviour
+{
+    #region +
+    [HideInInspector]
+    public int numberValue = 0;//数字的值
+    [HideInInspector]
+    public int Xposition = 0;//X轴位置---方格中的位置
+    [HideInInspector]
+    public int Yposition = 0;//Y轴位置
+    [HideInInspector]
+    public bool hasMixed = false;//表示数字是否完成过一次合并
+    #endregion
+
+    #region -
+    private UISprite mySprite = null;//NGUI的图片
+    private TweenPosition tweenPosition = null;//NGUI的动画
+    private bool isMoving;//保证只执行一次移动动画
+    private bool ToDestroy = false;//判断数字是否在移动完成之后需要被删除
+    #endregion
+
+    #region Unity内置函数
+    void Start()
+    {
+        tweenPosition = this.GetComponent<TweenPosition>();
+        mySprite = this.GetComponent<UISprite>();
+        numberValue = Random.value > 0.36f ? 2 : 4;//随机生成不一样的数字
+        mySprite.spriteName = numberValue.ToString();//设置图片
+
+        do
+        {
+            Xposition = Random.Range(0, 4);
+            Yposition = Random.Range(0, 4);
+        }
+        while (Manager._i.Numbers[Xposition, Yposition] != null);//如果随即到的那个格子已经有数字了的话就重新随机一下位置
+
+        transform.localPosition = new Vector2(-150 + Xposition * 100, -115 + Yposition * 100);//在方格中的实际位置
+        Manager._i.Numbers[Xposition, Yposition] = this;//将自己放入那个格子中
+
+        if (Manager._i.isDead())//判断游戏结束
+        {
+            Manager._i.GameLose();
+        }
+
+        //设置数字的起始位置，这样实例化一个数字的时候开始就不会移动了
+        tweenPosition.from = new Vector2(-150 + Xposition * 100, -115 + Yposition * 100);
+        tweenPosition.to = new Vector2(-150 + Xposition * 100, -115 + Yposition * 100);
+    }
+    void Update()
+    {
+        if (!isMoving)//只有不是移动中才能进行移动
+        {
+            //只有当前的位置不等于目标位置才能进行移动
+            if (transform.localPosition != new Vector3(-150 + Xposition * 100, -115 + Yposition * 100, 0))
+            {
+                tweenPosition.from = transform.localPosition;//设置开始位置
+                tweenPosition.to = new Vector3(-150 + Xposition * 100, -115 + Yposition * 100, 0);//设置目标位置
+                tweenPosition.ResetToBeginning();//初始化动画重头开始播放
+                tweenPosition.PlayForward();//开启正常的向前播放动画
+                isMoving = true;//设置为移动中
+            }
+        }
+    }
+    #endregion
+
+    #region +Move实际的移动方法
+    public void Move(int directionX, int directionY)
+    {
+        if (directionX == 1)
+        {
+            int index = 1;
+
+            while (Manager._i.isEmpty(Xposition + index, Yposition))
+            {
+                index++;
+            }
+
+            if (index > 1)//移动
+            {
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                Manager._i.Numbers[Xposition, Yposition] = null;//先讲本身的位置置为空
+                Xposition = Xposition + index - 1;//重新设置本身数字的X轴上的位置
+                Manager._i.Numbers[Xposition, Yposition] = this;//重新设置一下在数组里的对应关系
+            }
+
+            //判断值相同就合并
+            if (Xposition < 3 && numberValue == Manager._i.Numbers[Xposition + 1, Yposition].numberValue && !Manager._i.Numbers[Xposition + 1, Yposition].hasMixed)
+            {
+                Manager._i.Numbers[Xposition + 1, Yposition].hasMixed = true;
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                ToDestroy = true;//相同是就要删除了
+                Manager._i.Numbers[Xposition + 1, Yposition].numberValue *= 2;
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Xposition += 1;
+            }
+
+        }
+        else if (directionX == -1)
+        {
+            int index = 1;
+            while (Manager._i.isEmpty(Xposition - index, Yposition))
+            {
+                index++;
+            }
+
+            if (index > 1)
+            {
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Xposition = Xposition - index + 1;
+                Manager._i.Numbers[Xposition, Yposition] = this;
+            }
+
+            if (Xposition > 0 && numberValue == Manager._i.Numbers[Xposition - 1, Yposition].numberValue && !Manager._i.Numbers[Xposition - 1, Yposition].hasMixed)
+            {
+                Manager._i.Numbers[Xposition - 1, Yposition].hasMixed = true;
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                ToDestroy = true;
+                Manager._i.Numbers[Xposition - 1, Yposition].numberValue *= 2;
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Xposition -= 1;
+            }
+
+        }
+        else if (directionY == 1)
+        {
+            int index = 1;
+            while (Manager._i.isEmpty(Xposition, Yposition + index))
+            {
+                index++;
+            }
+
+            if (index > 1)
+            {
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Yposition = Yposition + index - 1;
+                Manager._i.Numbers[Xposition, Yposition] = this;
+            }
+
+            if (Yposition < 3 && numberValue == Manager._i.Numbers[Xposition, Yposition + 1].numberValue && !Manager._i.Numbers[Xposition, Yposition + 1].hasMixed)
+            {
+                Manager._i.Numbers[Xposition, Yposition + 1].hasMixed = true;
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                ToDestroy = true;
+                Manager._i.Numbers[Xposition, Yposition + 1].numberValue *= 2;
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Yposition += 1;
+            }
+
+        }
+        else if (directionY == -1)
+        {
+            int index = 1;
+            while (Manager._i.isEmpty(Xposition, Yposition - index))
+            {
+                index++;
+            }
+
+            if (index > 1)
+            {
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Yposition = Yposition - index + 1;
+                Manager._i.Numbers[Xposition, Yposition] = this;
+            }
+
+            if (Yposition > 0 && numberValue == Manager._i.Numbers[Xposition, Yposition - 1].numberValue && !Manager._i.Numbers[Xposition, Yposition - 1].hasMixed)
+            {
+                Manager._i.Numbers[Xposition, Yposition - 1].hasMixed = true;
+                Manager._i.hasMove = true;
+
+                //防止添加两个相同的数字
+                if (!Manager._i.IsMovingNumbers.Contains(this))
+                {
+                    Manager._i.IsMovingNumbers.Add(this);
+                }
+
+                ToDestroy = true;
+                Manager._i.Numbers[Xposition, Yposition - 1].numberValue *= 2;
+                Manager._i.Numbers[Xposition, Yposition] = null;
+                Yposition -= 1;
+            }
+        }
+    }
+    #endregion
+
+    #region +MoveOver移动结束后触发的事件
+    public void MoveOver()
+    {
+        isMoving = false;
+
+        if (ToDestroy)//删除当前的数字
+        {
+            Destroy(this.gameObject);
+
+            //合并后更改图片
+            Manager._i.Numbers[Xposition, Yposition].mySprite.spriteName = Manager._i.Numbers[Xposition, Yposition].numberValue.ToString();
+
+            //设置游戏胜利
+            if (Manager._i.Numbers[Xposition, Yposition].numberValue == 4096)
+            {
+                Manager._i.GameWin();
+            }
+        }
+
+        Manager._i.IsMovingNumbers.Remove(this);//移动之后需要删除
+    }
+    #endregion
+
+}
